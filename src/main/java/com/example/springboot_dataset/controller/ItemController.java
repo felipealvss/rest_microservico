8cb7db9ce469e8bf88f01b05c803e61a35b98831
Felipe Alves <lipealvessax@gmail.com>
2025-04-05 11:27:03 -0300

    Configuração de função getItemById

--- src/main/java/com/example/springboot_dataset/controller/ItemController.java
+++ src/main/java/com/example/springboot_dataset/controller/ItemController.java
@@ -3,7 +3,9 @@ package com.example.springboot_dataset.controller;
 import com.example.springboot_dataset.model.Item;
 import com.example.springboot_dataset.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class ItemController {
     @GetMapping
     public List<Item> getAllItems(){
         return itemService.getAllItems();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable String id){
+        return itemService.getItemById(id);
     }
 }
--- src/main/java/com/example/springboot_dataset/service/ItemService.java
+++ src/main/java/com/example/springboot_dataset/service/ItemService.java
@@ -3,6 +3,7 @@ package com.example.springboot_dataset.service;
 import com.example.springboot_dataset.model.Item;
 import com.example.springboot_dataset.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,5 +16,11 @@ public class ItemService {
 
     public List<Item> getAllItems(){
         return itemRepository.findAll();
+    }
+
+    public ResponseEntity<Item> getItemById(String id){
+        return itemRepository.findById(id)
+                .map(ResponseEntity :: ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
